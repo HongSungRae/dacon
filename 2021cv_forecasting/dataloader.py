51c752dba1ace74fab8764b5d646565421dec989
Hong <sr5043@naver.com>
2021-01-22 16:50:35 +0900

    dacon

---
+++ 2021cv_forecasting/dataloader.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from utils import *
+
+
+class MyDataLoader(Dataset):
+    def __init__(self,df):
+        super().__init__()
+        self.df = df
+        self.length = len(self.df) - 9*48 # Last 2days were used for forecasting only.
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self,idx):
+        start = idx*48
+        x = self.df.iloc[start:start+7*48,-1].values
+        y = self.df.iloc[start+7*48:start+9*48,-1].values
+        factor = self.df.iloc[start:start+7*48,3:7].values # [336,4]
+        factor = np.transpose(factor,(1,0))
+
+        x = torch.tensor(x).view(1,7,48)
+        y = torch.tensor(y).view(1,2,48)
+        factor = torch.tensor(factor).view(4,7,48)
+        return x, y, factor
+
+
+if __name__=="__main__":
+    df = pd.read_csv('/daintlab/data/sr/dacon/load_forecasting/train/train.csv')
+    data_set = MyDataLoader(df)
+    data_loader = DataLoader(data_set, shuffle=False, batch_size=64, pin_memory=False)
+    x, y, factor = next(iter(data_loader))
+
+    print("x : {}".format(x.shape))
+    print("y : {}".format(y.shape))
+    print("factor : {}".format(factor.shape))
---
+++ 2021cv_forecasting/model.py
@@ -0,0 +1,116 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+from torch.utils.data import DataLoader
+from dataloader import MyDataLoader
+
+
+
+
+class ForecastingCNN(nn.Module):
+  def __init__(self, in_channels = 1):
+    super().__init__()
+
+    self.cnn_channels = CNN_channels()
+    self.linear = nn.Linear(in_features=16125,out_features=48)
+
+
+  def forward(self,x,factor=torch.zeros([2,1,5,25]).cuda()): # factor = ID + Month + Day + Week
+    x = self.cnn_channels(x)
+    x = torch.cat([x,factor],dim=1)
+    x = x.reshape(x.shape[0],-1) # flatten
+    # x = torch.flatten(x)
+    x = self.linear(x)
+    return x
+
+
+class CNN_channels(nn.Module):
+  def __init__(self,dropout=.5):
+    super().__init__()
+
+    self.horizontal_channel = horizontal()
+    self.vertical_channel = vertical()
+    self.dropout = nn.Dropout(p=dropout)
+
+  def forward(self,x):
+    x1 = self.horizontal_channel(x)
+    x2 = self.vertical_channel(x)
+    x = torch.cat([x1,x2],dim=1)
+
+    return self.dropout(x)
+
+
+class horizontal(nn.Module):
+  def __init__(self, in_channels = 1):
+    super().__init__()
+
+    self.conv1 = conv_block(in_channels=in_channels, out_channels = 16, kernel_size=(1,7))
+    self.conv2 = conv_block(16, 24, kernel_size=(1,5))
+    self.maxPool1 = nn.MaxPool2d(kernel_size=(1,2),stride=(1,1))
+    self.conv3 = conv_block(24, 24, kernel_size =(1,5))
+    self.maxPool2 = nn.MaxPool2d(kernel_size=(1,2),stride=(1,1))
+    self.conv4 = conv_block(24, 64, kernel_size =(1,4))
+    self.maxPool3 = nn.MaxPool2d(kernel_size=(2,1),stride=(1,1))
+    self.conv5 = conv_block(64, 64, kernel_size =(1,3))
+    self.maxPool4 = nn.MaxPool2d(kernel_size=(2,1),stride=(1,1))
+    self.conv6 = conv_block(64, 64, kernel_size =(1,3))
+
+  def forward(self,x):
+    x = self.conv1(x)
+    x = self.conv2(x)
+    x = self.maxPool1(x)
+    x = self.conv3(x)
+    x = self.maxPool2(x)
+    x = self.conv4(x)
+    x = self.maxPool3(x)
+    x = self.conv5(x)
+    x = self.maxPool4(x)
+    x = self.conv6(x)
+    return x
+
+
+class vertical(nn.Module):
+  def __init__(self, in_channels = 1):
+    super().__init__()
+
+    self.conv1 = conv_block(in_channels=in_channels, out_channels=16, kernel_size=(4,1),padding=(8,29))
+    self.maxPool1 = nn.MaxPool2d(kernel_size=(1,2))
+    self.conv2 = conv_block(16, 24, kernel_size=(4,1))
+    self.maxPool2 = nn.MaxPool2d(kernel_size=(1,2))
+    self.conv3 = conv_block(24, 24, kernel_size=(3,1))
+    self.conv4 = conv_block(24, 64, kernel_size=(3,1))
+    self.maxPool3 = nn.MaxPool2d(kernel_size=(1,2),stride=(1,1))
+    self.conv5 = conv_block(64, 64, kernel_size=(2,1))
+    self.maxPool4 = nn.MaxPool2d(kernel_size=(2,1))
+    self.conv6 = conv_block(64, 64, kernel_size=(2,1))
+
+  def forward(self,x):
+    x = self.conv1(x)
+    x = self.maxPool1(x)
+    x = self.conv2(x)
+    x = self.maxPool2(x)
+    x = self.conv3(x)
+    x = self.conv4(x)
+    x = self.maxPool3(x)
+    x = self.conv5(x)
+    x = self.maxPool4(x)
+    x = self.conv6(x)
+    return x
+
+
+class conv_block(nn.Module):
+  def __init__(self, in_channels, out_channels, **kwargs):
+    super().__init__()
+    self.relu = nn.ReLU()
+    self.conv = nn.Conv2d(in_channels,out_channels,**kwargs)
+
+  def forward(self,x):
+    return self.relu(self.conv(x))
+
+
+
+
+if __name__ == "__main__":
+    net = ForecastingCNN().cuda()
+    print('======## ForecastingCNN Network ##======')
+    summary(net,input_size=(1,7,48))
