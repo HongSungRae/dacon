ee0559a58fa7d25baf048800c6a1427fbd0b4055
Hong <sr5043@naver.com>
2021-01-22 15:46:40 +0900

    dacon

---
+++ 2021cv_forecasting/loss.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+class Pinball_loss(nn.Module):
+    def __init__(self,quantile):
+        super().__init__()
+        self.quantile = quantile
+
+    def forward(self,y,y_pred):
+        # if *view* is needed, code it here
+        return (y-y_pred)*self.quantile if y-y_pred>=0 else (y_pred-y)*(1-self.quantile)
+
+
+if __name__ == "__main__":
+    y = torch.randn(64,1,96)
+    y_pred = torch.randn(64,1,96)
+    quantile = 0.5
+
+    criterion = Pinball_loss(quantile)
+    loss = criterion(y,y_pred)
+    print(loss)
